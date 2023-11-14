d81f0acc489441cd257cfefc70dd019271e959f1
Ryan Byrnes (Class of 2026) <260220rb@internal.salesianum.org>
2023-11-14 14:51:20 -0500

    commiting to merge main

---
+++ guessinggame.py
@@ -0,0 +1,12 @@
+def main():
+    again=str(input("Do you want to play again yes/no?"))
+    if again == "yes":
+        input("Enter a number between 1-100")
+    if again == "no":
+        print("thank you for playing")
+
+
+
+
+if __name__ == "__main__":
+ main()
