99db9aab783ecb43f1fd84e388fe11d812b42935
Braydon Kropp (Class of 2026) <261000bk@internal.salesianum.org>
2023-11-14 15:02:06 -0500

    finished code but need to make changes idk

--- mainCode.py
+++ mainCode.py
@@ -1,11 +1,15 @@
 import random
 def main():
+    playagain()
+
+def playagain():
     randNumb = random.randint(1,99)
     list = [1, 2, 3]
     for range in list:
         guess = player_guess()
         give_feedback(randNumb, guess)
     print("The number was " + str(randNumb))
+    restart()
 
 def player_guess():
     return input("Guess a number 1-100: ")
@@ -17,6 +21,21 @@ def give_feedback(number, guess):
         print("Higher!")
     if (int(guess) == int(number)):
         print("You Win!")
+        if (input("Would you like to play again? ") == "yes"):
+            print("play again")
+            playagain()
+        if (input("Would you like to play again? ") == "no"):
+            print("bye")
+
+def restart():
+    restartVar = input("Would you like to play again? ")
+    if (restartVar == "yes"):
+        playagain()
+    elif (restartVar == "no"):
+        print("Adios!")
+    else:
+        print("?")
+        restart()
 
 if __name__ == "__main__":
     main()
