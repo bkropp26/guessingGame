26378a6c26325b4cb4fe05a4ad6cf4bccde7ebb8
Braydon Kropp (Class of 2026) <261000bk@internal.salesianum.org>
2023-11-16 12:26:56 -0500

    fixed code and added while loop

--- Working+numgen.py
+++ Working+numgen.py
@@ -3,13 +3,15 @@ def main():
     playagain()
     randomnumber()
 def playagain():
+    restartVar = "yes"
     secretnumber = randomnumber()
     list = [1, 2, 3]
-    for range in list:
-        guess = player_guess()
-        give_feedback(secretnumber, guess)
-    print("The number was " + str(secretnumber))
-    restart()
+    while restartVar == "yes":
+        for range in list:
+            guess = player_guess()
+            give_feedback(secretnumber, guess)
+        print("The number was " + str(secretnumber))
+        restartVar = input("Would you like to play again? ")
 
 def player_guess():
     return input("Guess a number 1-100: ")
@@ -30,16 +32,6 @@ def give_feedback(randomnumber, guess):
             playagain()
         if (input("Would you like to play again? ") == "no"):
             print("bye")
-
-def restart():
-    restartVar = input("Would you like to play again? ")
-    if (restartVar == "yes"):
-        playagain()
-    elif (restartVar == "no"):
-        print("Adios!")
-    else:
-        print("?")
-        restart()
 
 if __name__ == "__main__":
     main()
